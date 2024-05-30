be8235e9cfe10cb37d784a282e5324f9f39b8d62
Carlos Toledo <carlombo@hotmail.com>
2024-05-30 19:47:45 -0400

    Se agrega un diccionario con mascotas y se intera los datos en pantalla

--- mascotas/views.py
+++ mascotas/views.py
@@ -2,10 +2,30 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 # Create your views here.
+mascotas = [
+    {
+        'id': 1,
+        'nombre': 'Pelusa',
+        'especie': 'gato'
+    },
+    {
+        'id': 2,
+        'nombre': 'Rocky',
+        'especie': 'perro'
+    },
+    {
+        'id': 3,
+        'nombre': 'Nemo',
+        'especie': 'pez'
+    },
+]
+
+
 def paginaMascotas(request):
     context = {
         'titulo': 'Este es el Portal de las Mascotas',
-        'esAdmin': False,
-        'nombre': 'Matias Bensan'
+        'esAdmin': True,
+        'nombre': 'Matias Bensan', 
+        'mascotas': mascotas
     }
     return render(request, 'mascotas.html', context)
